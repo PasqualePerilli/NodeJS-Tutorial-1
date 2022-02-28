46084b724a358c9ea663b4a941a4d2bb2bd842ee
PasqualePerilli <pasqualeperilli@protonmail.com>
2022-02-28 02:15:18 -0600

    Updated asyncAwaitBookApi to introduce both valid and invalid responses. Prettied code up

--- CD220Labs/async_callback/asyncAwaitBookApi.js
+++ CD220Labs/async_callback/asyncAwaitBookApi.js
@@ -1,12 +1,34 @@
-const axios = require('axios').default;
-const connectToURL = async(url)=>{
+const useValidURL = false; // Set to true to see a valid response. To false to see an error
+
+const axios         = require('axios').default;
+const webSite       = "https://reststop.randomhouse.com/";
+const urlPath       = "resources/works/?expandLevel=1&search=";
+const realAuthor    = "Grisham";
+const fakeAuthor    = "IDontExist.html";
+const tab           = "\t";
+const newLine       = "\n";
+const correctURL    = webSite + urlPath + realAuthor;
+const fakeURL       = webSite + fakeAuthor;
+var   urlToUse      = useValidURL == true ? correctURL : fakeURL;
+
+
+const connectToURL  = async(url) => {
+    console.log("Attempting to send request to URL [" + url + "]");
     const outcome = axios.get(url);
     let listOfWork = (await outcome).data.work;
+    console.log("Obtained response from URL [" + url + "].");
+    console.log("Proceeding to print all titles below:" + newLine);
     listOfWork.forEach((work)=>{
-      console.log(work.titleAuth);
+      console.log(tab + work.titleAuth);
     });
+    console.log(newLine + "Finished attempt to send request to URL [" + url + "]" + newLine);
 }
 
-console.log("Before connect URL")
-connectToURL('https://reststop.randomhouse.com/resources/works/?expandLevel=1&search=Grisham').catch(err=>console.log(err.toString()));
-console.log("After connect URL")
+console.log(newLine + "Before connecting to URL [" + urlToUse + "]");
+connectToURL(urlToUse).catch( error => {
+  console.log("An error occurred while attempting to connect to URL [" + urlToUse + "]. Printing details below:");
+  console.log(newLine + tab + error.toString() + newLine);
+});
+console.log(newLine + "After connecting to URL [" + urlToUse + "]" + newLine);
+console.log("--------------------------------------------------------");
+console.log(newLine);
